22c7ca093ef5d0cdb6a72db8c228308fcb549e03
fdlii <155849941+fdlii@users.noreply.github.com>
2024-03-03 12:42:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,66 @@
 
 
 double pown(double value, uint16_t n) {
-
+    if (n == 0) return 1;
+    uint16_t num = 1;
+    double res = value;
+    if (n > 0) {
+        while (num != n) {
+            res *= value;
+            num++;
+        }
+    }
+    return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+    uint16_t num = 1;
+    uint64_t res = 1;
+    if (n == 0) return 1;
+    else {
+        while (num != n) {
+            num += 1;
+            res *= num;
+        }
+    }
+    return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+    return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-
+    double res = 1;
+    if (count == 0) return 0;
+    uint16_t i = 1;
+    while (count != i) {
+        res += calcItem(x, i);
+        i++;
+    }
+    return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+    double res = 0;
+    if (count == 0) return 0;
+    uint16_t i = 1;
+    while (count != i) {
+        res = res + calcItem(x, 2 * i - 1) * pown(-1, i - 1);
+        i++;
+    }
+    return res;
 }
 
 double cosn(double x, uint16_t count) {
-
+    double res = 0;
+    if (count == 0) return 0;
+    if (count == 1) return x;
+    uint16_t i = 1;
+    while (count != i) {
+        res = res + calcItem(x, 2 * i - 2) * pown(-1, i - 1);
+        i++;
+    }
+    res = res + calcItem(x, 2 * i - 2) * pown(-1, i - 1);
+    return res;
 }
